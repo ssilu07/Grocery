f074844e3c6aa4082d882eb11d0b7d77c0dd179c
sumit <sumits7196@gmail.com>
2021-03-17 00:37:49 +0530

    Grocery app

---
+++ app/src/main/java/com/study/grocery/Constants.java
@@ -0,0 +1,37 @@
+package com.study.grocery;
+
+public class Constants {
+
+    public static final String FCM_KEY = "AAAAbPEP1Z0:APA91bGv2mo24k6WMmrPTexcft1DVPprpuerMLuacZz83btwpbAed8_XQnk3Vjwq9aXA50C2f-7bAEXLnTWzXbApledMrEb1CvE8AfSULWWLsKla7iP-n4qZOqD80wvwX-1DxGUP-jt7";
+    public static final String FCM_TOPIC = "PUSH_NOTIFICATIONS";
+
+    //product Categories
+    public static final String[] productcategories = {
+            "Beverages",
+            "Beauty & skin",
+            "Biscuits",
+            "Breakfast",
+            "Cooking Needs",
+            "Frozen Food",
+            "Fruits",
+            "pharmacy",
+            "vegetables",
+            "others"
+
+    };
+
+    public static final String[] productcategories1 = {
+            "All",
+            "Beverages",
+            "Beauty & skin",
+            "Biscuits",
+            "Breakfast",
+            "Cooking Needs",
+            "Frozen Food",
+            "Fruits",
+            "pharmacy",
+            "vegetables",
+            "others"
+
+    };
+}
